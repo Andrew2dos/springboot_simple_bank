e3be6923652c1887b9dc91972afa414cb269455e
Andrew <andrew2dos@gmail.com>
2022-05-22 21:34:55 +0300

    simple api without db

---
+++ src/test/java/com/andrew2dos/simplebank/BankServiceTest.java
@@ -0,0 +1,19 @@
+package com.andrew2dos.simplebank;
+
+class BankServiceTest {
+
+//    private BalanceRepository balanceRepository;
+//    private BankService bankService = new BankService(balanceRepository);
+//
+//    @Test
+//    void getBalance() {
+//        BigDecimal balance = bankService.getBalance(1L);
+//        assertEquals(balance, BigDecimal.TEN);
+//    }
+//
+//    @Test
+//    void addMoney() {
+//        BigDecimal balance = bankService.addMoney(1L, BigDecimal.ONE);
+//        assertEquals(balance, BigDecimal.valueOf(11));
+//    }
+}
---
+++ src/test/java/com/andrew2dos/simplebank/MoneyTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew2dos.simplebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
